aa5a05f890d56656355f907f4a364721cb3a9553
Tanchereau <victor.tanchereau@gmail.com>
2016-12-12 11:07:51 +0100

    Ajout du script exercice_produits

---
+++ edt_insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO Formation (nom_formation, nbHeuresTotal_formation) VALUES ('TOTO', '13');
+
+INSERT INTO Promotion (id_formation, nom_promotion, dateDebut_promotion, dateFin_promotion) VALUES (1, 'tata', '20161010 00:00:00 AM', '20161010 00:00:00 AM');
---
+++ exercice_produits.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Categorie(
+	libelle VARCHAR (25)
+);
+
+CREATE TABLE Produit (
+	nom VARCHAR (25),
+	code CHAR(5)
+);
+
+ALTER TABLE Categorie ADD id INTEGER NOT NULL IDENTITY(1,1);
+ALTER TABLE Categorie 
+	ADD CONSTRAINT categorie_pk
+	PRIMARY KEY (id);
+
+ALTER TABLE Produit ADD id INTEGER NOT NULL IDENTITY(1,1);
+ALTER TABLE Produit 
+	ADD CONSTRAINT produit_pk
+	PRIMARY KEY (id);
+ALTER TABLE Produit 
+	ADD CONSTRAINT produit_fk
+	FOREIGN KEY (id)
+	REFERENCES Categorie (id);
